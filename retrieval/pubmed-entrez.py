142c6c00aa344fe05d41004c9a87f49e2daba7fb
Alex Purdy <apurdy@uvic.ca>
2014-10-01 16:05:46 -0700

    Fetch script refactoring and documentation

--- retrieval/pubmed-entrez.py
+++ retrieval/pubmed-entrez.py
@@ -1,15 +1,61 @@
+from http.client import IncompleteRead
 import os
 from Bio import Entrez
 import gzip
+import logging
 
-# Usage limits Entrez Link
-# http://eutils.ncbi.nlm.nih.gov/entrez/query/static/entrezlinks.html#pmc
+logging.basicConfig(filename='fetch.log', level=logging.DEBUG)
 
-if __name__ == '__main__':
-    Entrez.email = "apurdy@uvic.ca"
-    path = '/home/alex/medline/'
-    records_per_year = {}
-    for year in range(1865, 2017, 1):
+
+# todo: fix messy parameters
+# todo: elink queries are expensive, returns unnecesary data
+def fetch_urls(outdir, webenv, query_key, count, batch_size=100):
+    """Retrieves associated urls for a set of medline records.
+
+    Retrieves the associated full-text source urls using the Entrez Link
+    service. Server request limits are enforced by the Entrez module.
+
+    Args:
+        webenv: Web environment string returned from a previous ESearch, EPost or
+        ELink call. When provided, ESearch will post the results of the search
+        operation to this pre-existing WebEnv, thereby appending the results to
+        the existing environment.
+        query_key: Integer query key returned by a previous ESearch, EPost or ELink
+        call. When provided, ESearch will find the intersection of the set specified
+        by query_key and the set retrieved by the query in term (i.e. joins the two
+        with AND).
+        batch_size: Total number of DocSums from the input set to be retrieved,
+        up to a maximum of 10,000.
+    Returns:
+        Handle containing response in XML format.
+
+    Raises:
+        IncompleteRead: An error occurred reading data through the HTTP connection.
+    """
+    for start in range(0, count, batch_size):
+        end = min(count, start + batch_size)
+        with gzip.open(os.join(outdir, "{0:d}-{1:d}.links.xml.gz".format(start + 1, end)), 'w') as out:
+            logging.info("Fetching associated links for record %i to %i" % (start + 1, end))
+            linkout_handle = Entrez.elink(dbfrom="pubmed",
+                                          retmode="xml",
+                                          restart=start, retmax=batch_size,
+                                          cmd="llinks",
+                                          webenv=webenv,
+                                          query_key=query_key,
+                                          linkname="pubmed_pubmed")
+            if int(linkout_handle['Count']) == 0:
+                continue
+            try:
+                data = linkout_handle.read()
+            except IncompleteRead:
+                logging.error("Fail to read records %i to %i" % (start + 1, end))
+            else:
+                out.write(data)
+                linkout_handle.close()
+
+
+def fetch_records_by_year(start, stop, fetch_links=False):
+    for year in range(start, stop, 1):
         search_handle = Entrez.esearch(db="pubmed",
                                        term="{0:d}[crdt]".format(year),
                                        usehistory='y')
@@ -17,8 +63,7 @@ if __name__ == '__main__':
         search_handle.close()
 
         count = int(search_results['Count'])
-        print("Found {0:d} results for year {1:d}".format(count, year))
-        records_per_year[year] = count
+        logging.info("Found {0:d} results for year {1:d}".format(count, year))
         if count == 0:
             continue
 
@@ -29,41 +74,26 @@ if __name__ == '__main__':
 
         for start in range(0, count, batch_size):
             end = min(count, start + batch_size)
-            with gzip.open(os.path.join(records_dir_path, "{0:d}-{1:d}.xml.gz".format(start + 1, end)),
-                           "wt") as raw_handle:
-                print("Going to download record %i to %i" % (start + 1, end))
+            with gzip.open(os.path.join(records_dir_path, "{0:d}-{1:d}.xml.gz".format(start + 1, end)), "wt") as out:
+                logging.info("Fetching medline records %i to %i" % (start + 1, end))
                 fetch_handle = Entrez.efetch(db="pubmed",
                                              rettype="medline", retmode="xml",
                                              restart=start, retmax=batch_size,
                                              webenv=search_results["WebEnv"],
                                              query_key=search_results["QueryKey"])
-                data = fetch_handle.read()
-                fetch_handle.close()
-                raw_handle.write(data)
+                try:
+                    data = fetch_handle.read()
+                except IncompleteRead:
+                    logging.error("Fail to read records %i to %i" % (start + 1, end))
+                else:
+                    out.write(data)
+                    fetch_handle.close()
+        if fetch_links:
+            fetch_urls(records_dir_path, webenv=search_results["WebEnv"], query_key=search_results["QueryKey"], count=count)
 
-                # batch_size = 5000
-                # with open('store/urls.txt', 'w') as url_handle:
-                # for start in range(0, count, batch_size):
-                # end = min(count, start + batch_size)
-                # print("Fetching associated links for record %i to %i" % (start + 1, end))
-                # linkout_handle = Entrez.elink(dbfrom="pubmed",
-                #                                       retmode="xml",
-                #                                       restart=start, retmax=batch_size,
-                #                                       cmd="llinks",
-                #                                       webenv=search_results["WebEnv"],
-                #                                       query_key=search_results["QueryKey"],
-                #                                       linkname="pubmed_pubmed")
-                #         records = Entrez.read(linkout_handle)
-                #         linkout_handle.close()
-                #         id_url_set = records[0]['IdUrlList']['IdUrlSet']
-                #         for id_url in id_url_set:
-                #             pid = id_url['Id']
-                #             urls = set()
-                #             for obj_url in id_url['ObjUrl']:
-                #                 if 'Full Text Sources' in obj_url['Category'] and all(
-                #                                 k in obj_url['Attribute'] for k in ('free resource', 'full-text online')):
-                #                     urls.add(obj_url['Url'])
-                #             if (urls):
-                #                 url_handle.write('>' + pid + '\n' + str('\n'.join(urls)) + '\n')
-                #
-    print(records_per_year)
+
+if __name__ == '__main__':
+    Entrez.email = "apurdy@uvic.ca"
+    path = '/home/alex/medline/'
+    fetch_records_by_year(1952, 2017, fetch_links=False)
+
