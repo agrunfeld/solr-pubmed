4d5b5d03c428403d8b22caf1db3788dada8d9958
Alex Purdy <apurdy@uvic.ca>
2014-10-08 21:49:57 -0700

    Missing files

---
+++ ingestion/src/main/java/pubmed/Date.java
@@ -0,0 +1,13 @@
+package pubmed;
+
+import java.text.ParseException;
+
+public interface Date {
+    public Year getYear();
+    public Month getMonth();
+    public Day getDay();
+    public void setYear(Year year);
+    public void setMonth(Month month);
+    public void setDay(Day day);
+    public java.util.Date getConvertedDate() throws ParseException;
+}
